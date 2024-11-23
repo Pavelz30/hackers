f12c22dee224f93304f27b3d489f4f3004916afc
Pavel Zavodyan <pavel.zavodyan@mail.ru>
2024-11-23 10:02:07 +0000

    add data.json

--- app.py
+++ app.py
@@ -6,25 +6,58 @@ import vulners
 import os
 from fpdf import FPDF
 import translator
+from pymongo import MongoClient
 
 
 def get_vulnerability_description(vulners_api: vulners, vulnerability_ids):
     return vulners_api.get_multiple_bulletins(id=vulnerability_ids, fields=["description"])
 
+def insert_data(nm: nmap.PortScanner, db):
+    document_info = {}
+    for proto, data in nm.scaninfo().items():
+        document_info[proto] = data['services']
+    document_info['timestr'] = nm.scanstats()['timestr']
+    db.scans.insert_one(document_info)
+
+    print(nm.all_hosts())
+
+    for host in nm.all_hosts():
+        for proto in nm[host].all_protocols():
+            lport = nm[host][proto].keys()
+            for port in lport:
+                document = {
+                    'ip': host,
+                    'hostname': host['hostnames']['name']
+                }
+            print ('port : %s\tstate : %s' % (port, nm[host][proto][port]['state']))
+    # for ip, data in nm.:
+    #     document = {
+    #         "ip": ip,
+    #         "hostnames": data['hostnames'],
+    #         "addresses": data['addresses'],
+    #         "status": data['status'],
+    #         "tcp": data.get('tcp', {})
+    #     }
+    #     db.scans.insert_one(document)
+
+
     
 def main():
+    client = MongoClient('localhost', 27017)
+    db = client['scans']
 
     vulners_api = vulners.Vulners(api_key=os.getenv('API_KEY'))
 
     nm = nmap.PortScanner()
-    nm.scan(hosts='45.67.229.226', arguments='-sS -F -sV --script vulners', sudo=True)
-    
+    nm.scan(hosts='45.67.229.226-227', arguments='-sS -F -sV --script vulners', sudo=True)
+    insert_data(nm, db)
     with open("data.json", "w", encoding="utf-8") as filewrite:
         json.dump(nm.analyse_nmap_xml_scan(), filewrite, ensure_ascii=False, indent=4)
     
     with open("data.json", 'r', encoding='utf-8') as fileread:
         data = json.load(fileread)
-        
+
+
     # lis = ["CVE-2023-6548", "CVE-2023-6549"]
     # descriptions= get_vulnerability_description(vulners_api, lis)
     # for cve in descriptions:
@@ -45,35 +78,6 @@ def main():
     #         ]
     #     }
     # """
-    # data = json.loads(json_data)
-    pdf = FPDF()
-    pdf.add_page()
-    pdf.set_font("Arial", size=14)
-
-    pdf.cell(200, 10, txt="Отчет о тетсировании", ln=True, align='C')
-    pdf.ln(10)
-    for proto in nm.scaninfo():
-        ports = nm.scaninfo()[proto]['services']
-        pdf.cell(200, 10, txt=ports, ln=True, align='C')
-    # # Добавляем пространство
-    # pdf.ln(10)
-
-    # def add_row(row_data):
-    #     for item in row_data:
-    #         pdf.cell(40, 10, txt=item, border=1)
-    #     pdf.ln()
-
-    # # Печать заголовка таблицы
-    # add_row(data['header'])
-
-    # # Печать данных
-    # for row in data['data']:
-    #     add_row([row['ID'], row['Name'], row['Value']])
-
-    # Сохраняем PDF
-    pdf_output_path = "output.pdf"
-    pdf.output(pdf_output_path)
-    print(f"PDF успешно создан: {pdf_output_path}")
 
 
 if __name__ == '__main__':
